d64d5b7e2c8e635c3cce87f211030e3fca4a0f58
sagarsavaliya <savaliya.sagar07@gmail.com>
2026-02-19 21:44:00 +0530

    resolving cors error

--- apps/api/config/cors.php
+++ apps/api/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://vastraaos.pages.dev', 'https://vastraaos-landing.pages.dev', 'http://localhost:5173', 'http://127.0.0.1:5173' ,'http://localhost:5174','http://localhost:5175'],
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'https://vastraaos.pages.dev,https://vastraaos-landing.pages.dev,http://localhost:5173,http://127.0.0.1:5173,http://localhost:5174,http://localhost:5175')),
 
     'allowed_origins_patterns' => [],
 
